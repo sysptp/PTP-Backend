58d005afc8c680f968a62cba64c5ac8e53abc89c
Gerald Antonio Silverio Serrata <20221068@itla.edu.do>
2024-10-31 22:40:33 -0400

    Creacion de repositorio de facturas.

---
+++ BussinessLayer/Repository/RFacturacion/InvoiceRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataLayer.Data;
+using DataLayer.Models.Facturas;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace BussinessLayer.Repository.RFacturacion
+{
+    public class InvoiceRepository(IConfiguration configuration) : IInvoiceRepository
+    {
+        private readonly IConfiguration _configuration = configuration;
+        public Task AddAsync(Facturacion invoice)
+        {
+            try
+            {
+                using SqlConnection context = new(_configuration.GetConnectionString("POS_CONN"));
+                const string query = @"INSERT INTO";
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(ex.Message, ex);
+            }
+        }
+
+        public Task<List<Facturacion>> GetAllAsync(int bussinesId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Facturacion> GetByIdAsync(string invoiceNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAync(Facturacion invoice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
