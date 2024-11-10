53564db5254ab9a7884aeb9f8815dfb65ac74e00
AntPOS0010 <elanthony095@gmail.com>
2024-11-09 22:47:40 -0400

    PDbContext para todas las entidades

---
+++ BussinessLayer/Repository/HelpDesk/HdkCategoryTicketRepository.cs
@@ -0,0 +1,20 @@
+﻿using BussinessLayer.Interfaces.Repository.HelpDesk;
+using BussinessLayer.Repository.ROtros;
+using DataLayer.Models.HelpDesk;
+using DataLayer.PDbContex;
+
+
+namespace BussinessLayer.Repository.HelpDesk
+{
+    public class HdkCategoryTicketRepository : GenericRepository<HdkCategoryTicket>, IHdkCategoryTicketRepository
+    {
+        private readonly PDbContext _dbContext;
+        private readonly ITokenService _tokenService;
+        public HdkCategoryTicketRepository (PDbContext dbContext, ITokenService tokenService) : base(dbContext, tokenService)
+        {
+            _dbContext = dbContext;
+            _tokenService = tokenService;
+        }
+
+    }
+}
---
+++ BussinessLayer/Repository/HelpDesk/HdkDepartXUsuarioRepository.cs
@@ -0,0 +1,19 @@
+﻿using BussinessLayer.Interfaces.Repository.HelpDesk;
+using BussinessLayer.Repository.ROtros;
+using DataLayer.Models.HelpDesk;
+using DataLayer.PDbContex;
+
+
+namespace BussinessLayer.Repository.HelpDesk
+{
+    public class HdkDepartXUsuarioRepository : GenericRepository<HdkDepartXUsuario>, IHdkDepartXUsuarioRepository
+    {
+        private readonly PDbContext _dbContext;
+        private readonly ITokenService _tokenService;
+        public HdkDepartXUsuarioRepository(PDbContext dbContext, ITokenService tokenService) : base(dbContext, tokenService)
+        {
+            _dbContext = dbContext;
+            _tokenService = tokenService;
+        }
+    }
+}
