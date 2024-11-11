d8a6b31ae8047e24fd9aa3777f1fabbb89cb9d1c
AntPOS0010 <elanthony095@gmail.com>
2024-11-11 19:31:44 -0400

    Creado Interfaz de HeplDesk Y una implementacion en Servicio

---
+++ BussinessLayer/Services/SHelpDesk/HdkCategoryTicketService.cs
@@ -0,0 +1,12 @@
+﻿using DataLayer.Models.HelpDesk;
+using BussinessLayer.Interfaces;
+using AutoMapper;
+using BussinessLayer.Interfaces.Repository.HelpDesk;
+using BussinessLayer.DTOs.HelpDesk;
+
+namespace BussinessLayer.Services.SHelpDesk
+{
+    internal class HdkCategoryTicketService
+    {
+    }
+}
