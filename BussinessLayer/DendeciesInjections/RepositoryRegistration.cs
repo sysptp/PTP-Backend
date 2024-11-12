0040c3f3dfa122b585da2ca4578ee78045b22608
AntPOS0010 <elanthony095@gmail.com>
2024-11-11 20:41:55 -0400

    RepositoryRegistretion injecciones de dependencia Decorator

--- BussinessLayer/DendeciesInjections/RepositoryRegistration.cs
+++ BussinessLayer/DendeciesInjections/RepositoryRegistration.cs
@@ -2,6 +2,8 @@
 using BussinessLayer.Interfaces.Repository.Configuracion.Modulo;
 using BussinessLayer.Interfaces.Repository.Empresa;
 using BussinessLayer.Interfaces.Repository.Geografia;
+using BussinessLayer.Interfaces.Repository.HelpDesk;
+using BussinessLayer.Repository.HelpDesk;
 using BussinessLayer.Repository.RConfiguracion.Menu;
 using BussinessLayer.Repository.RConfiguracion.Modulo;
 using BussinessLayer.Repository.REmpresa;
@@ -34,6 +36,21 @@ namespace BussinessLayer.DendeciesInjections
             #region Configuracion
             services.AddTransient<IGnMenuRepository,GnMenuRepository>();
             services.AddTransient<IGnModuloRepository,GnModuloRepository>();
+            #endregion
+
+            #region HelpDesk
+            services.AddTransient<IHdkCategoryTicketRepository, HdkCategoryTicketRepository>();
+            services.AddTransient<IHdkDepartamentsRepository, HdkDepartamentsRepository>();
+            services.AddTransient<IHdkDepartXUsuarioRepository, HdkDepartXUsuarioRepository>();
+            services.AddTransient<IHdkErrorSubCategoryRepository, HdkErrorSubCategoryRepository>();
+            services.AddTransient<IHdkFileEvidenceTicketRepository, HdkFileEvidenceTicketRepository>();
+            services.AddTransient<IHdkNoteTicketRepository, HdkNoteTicketRepository>();
+            services.AddTransient<IHdkPrioridadTicketRepository, HdkPrioridadTicketRepository>();
+            services.AddTransient<IHdkSolutionTicketRepository, HdkSolutionTicketRepository>();
+            services.AddTransient<IHdkStatusTicketRepository, HdkStatusTicketRepository>();
+            services.AddTransient<IHdkSubCategoryRepository, HdkSubCategoryRepository>();
+            services.AddTransient<IHdkTicketsRepository, HdkTicketsRepository>();
+            services.AddTransient<IHdkTypeTicketRepository, HdkTypeTicketRepository>();
             #endregion
         }
     }
