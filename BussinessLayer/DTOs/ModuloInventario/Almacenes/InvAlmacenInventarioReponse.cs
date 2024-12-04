3c65869aeceb55ea5ab9bf486371e8d912837615
AntPOS0010 <elanthony095@gmail.com>
2024-12-04 02:20:43 -0400

    Reques y reponse almacenes

---
+++ BussinessLayer/DTOs/ModuloInventario/Almacenes/InvAlmacenInventarioReponse.cs
@@ -0,0 +1,18 @@
+﻿using BussinessLayer.DTOs.Otros;
+
+
+namespace BussinessLayer.DTOs.ModuloInventario.Almacenes
+{
+    public class InvAlmacenInventarioReponse:AuditableEntitiesReponse
+    {
+        public int Id { get; set; }
+        public int IdProducto { get; set; }
+        public int IdEmpresa { get; set; }
+        public int IdAlmacen { get; set; }
+        public int CantidadProducto { get; set; }
+        public int CantidadMinima { get; set; }
+        public string UbicacionExhibicion { get; set; }
+        public string UbicacionGuardada { get; set; }
+        public bool Activo { get; set; }
+    }
+}
---
+++ BussinessLayer/DTOs/ModuloInventario/Almacenes/InvAlmacenesRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BussinessLayer.DTOs.ModuloInventario.Almacenes
+{
+    public class InvAlmacenesRequest
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+        public long IdEmpresa { get; set; }
+        public int IdMunicipio { get; set; }
+        public int IdUsuario { get; set; }
+        public long IdSucursal { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public bool EsPrincipal { get; set; }
+    }
+}
