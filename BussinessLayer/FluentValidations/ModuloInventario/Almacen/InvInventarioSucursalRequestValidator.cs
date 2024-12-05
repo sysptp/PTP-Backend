ed7dc13ddc30f6e2574332bc785e9263b00a1a03
AntPOS0010 <elanthony095@gmail.com>
2024-12-04 22:17:05 -0400

    Validator creados

---
+++ BussinessLayer/FluentValidations/ModuloInventario/Almacen/InvInventarioSucursalRequestValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+using BussinessLayer.DTOs.ModuloInventario.Almacenes;
+
+namespace BussinessLayer.FluentValidations.ModuloInventario.Almacen
+{
+    public class InvInventarioSucursalRequestValidator : AbstractValidator<InvInventarioSucursalRequest>
+    {
+        public InvInventarioSucursalRequestValidator() { }
+    }
+}
