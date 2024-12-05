612b03a0758ab3e476b49e355274c7438b3afe03
AntPOS0010 <elanthony095@gmail.com>
2024-12-04 23:53:01 -0400

    Los profile verdugo lo va a dejar

---
+++ BussinessLayer/Mapping/ModuloInventario/Almacen/InvMovInventarioSucursalProfile.cs
@@ -0,0 +1,20 @@
+﻿using BussinessLayer.DTOs.Auditoria;
+using DataLayer.Models.Auditoria;
+using AutoMapper;
+using BussinessLayer.DTOs.ModuloInventario.Almacenes;
+using DataLayer.Models.ModuloInventario.Almacen;
+
+namespace BussinessLayer.Mapping.ModuloInventario.Almacen
+{
+    public class InvMovInventarioSucursalProfile : Profile
+    {
+        public InvMovInventarioSucursalProfile()
+        {
+
+            CreateMap<InvMovInventarioSucursalRequest, InvMovInventarioSucursal>().ReverseMap();
+
+            CreateMap<InvMovInventarioSucursalReponse, InvMovInventarioSucursal>().ReverseMap();
+
+        }
+    }
+}
---
+++ BussinessLayer/Mapping/ModuloInventario/Almacen/InvMovimientoAlmacenDetalleProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BussinessLayer.DTOs.ModuloInventario.Almacenes;
+using DataLayer.Models.ModuloInventario.Almacen;
+
+namespace BussinessLayer.Mapping.ModuloInventario.Almacen
+{
+    internal class InvMovimientoAlmacenDetalleProfile : Profile
+    {
+        public InvMovimientoAlmacenDetalleProfile()
+        {
+
+            CreateMap<InvMovimientoAlmacenDetalleRequest, InvMovimientoAlmacenDetalle>().ReverseMap();
+
+            CreateMap<InvMovimientoAlmacenDetalleReponse, InvMovimientoAlmacenDetalle>().ReverseMap();
+
+        }
+    }
+}
