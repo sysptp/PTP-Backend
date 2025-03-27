9eb2984bed6cb8cf1b56691f4d16697317e2f3f0
GeraldSilverio <es.geraldsilverio@gmail.com>
2025-03-27 08:13:42 -0400

    Agregando endpoint para envio de SMS y WhatsApp

--- PTP_API/Controllers/NotificationModule/NotificationController.cs
+++ PTP_API/Controllers/NotificationModule/NotificationController.cs
@@ -5,12 +5,26 @@ using Microsoft.AspNetCore.Mvc;
 [ApiController]
 public class NotificationController : ControllerBase
 {
+    private readonly ITwilioService _twilioService;
+    public NotificationController(ITwilioService twilioService)
+    {
+        _twilioService = twilioService;
+    }
+
     [HttpPost]
     [Route("send-email")]
     public async Task<IActionResult> SendEmail([FromBody] SendGridRequest request)
     {
         SendGridService sendGridService = new SendGridService();
         var response = await sendGridService.SendEmailAsync();
+        return Ok(response);
+    }
+
+    [HttpPost]
+    [Route("send-sms-whatsapp")]
+    public async Task<IActionResult> SendSmsWhatsapp([FromBody]  SendMessageDto sendMessageDto )
+    {
+        var response = await _twilioService.SendMessage(sendMessageDto);
         return Ok(response);
     }
 }
