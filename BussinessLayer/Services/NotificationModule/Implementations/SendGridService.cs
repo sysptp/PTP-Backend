1349ea0a115024c3daa8313a27a91df03b751cac
GeraldSilverio <es.geraldsilverio@gmail.com>
2025-03-31 16:57:32 -0400

    Comenzando el feature de SendGrid

---
+++ BussinessLayer/Services/NotificationModule/Implementations/SendGridService.cs
@@ -0,0 +1,27 @@
+﻿//using SendGrid;
+//using SendGrid.Helpers.Mail;
+
+//namespace BussinessLayer.Services.NotificationModule.Implementations
+//{
+//    public class SendGridService
+//    {
+//        public async Task SendEmailAsync()
+//        {
+//            try
+//            {
+//                string apiKey = "";
+
+//                SendGridClient sendGridClient = new SendGridClient(apiKey);
+
+//                var message = new SendGridMessage()
+//                {
+//                    From 
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+//                throw new ApplicationException(ex.Message, ex);
+//            }
+//        }
+//    }
+//}
